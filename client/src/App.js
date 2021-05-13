1622781add8c7d25c081e30d1ed8e0db203f4d7c
Kwon Hyeok Jin <lexky@naver.com>
2021-05-13 17:49:17 +0900

    다른 유저 프로필 컴포넌트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import FriendSearch from "./Compontents/FriendSearch";
 import Myprofile from "./Compontents/Profile/Myprofile";
 import UploadStudyPost from "./Compontents/UploadStudyPost";
 import StudyDetail from "./Compontents/StudyDetail";
+import Profile from "./Compontents/Profile/Profile"
 import Auth from './hoc/auth'
 
 function App() {
@@ -21,13 +22,14 @@ function App() {
     <Suspense fallback={(<div>Loading...</div>)}>
       <NavBar />
       <div style={{ textAlign:'center', minHeight: 'calc(100vh - 80px)' }}>
-        <Route exact path="/" component={Auth(Mainpage, null)} /> {/* Main_Component */}
-        <Route exact path="/login" component={Auth(Login, false)} /> {/* Login_Component */}
-        <Route exact path="/signup" component={Auth(Regiseter, false)} /> {/* SignUp_Component */}
-        <Route exact path="/studySearch" component={Auth(StudySearch, true)} /> {/* StudySearch_Component */}
-        <Route exact path="/friendSearch" component={Auth(FriendSearch, true)} /> {/* FriendsSearch_Component */}
-        <Route exact path="/detail/:studyId" component={Auth(StudyDetail, true)} /> {/* studyDetail_Component */}
-        <Route exact path="/profile/:Id" component={Auth(Myprofile, true)} /> {/* friendPrifle_Component */}
+        <Route exact path="/" component={Auth(Mainpage, null)} /> 
+        <Route exact path="/login" component={Auth(Login, false)} /> 
+        <Route exact path="/signup" component={Auth(Regiseter, false)} />
+        <Route exact path="/studySearch" component={Auth(StudySearch, true)} />
+        <Route exact path="/friendSearch" component={Auth(FriendSearch, true)} /> 
+        <Route exact path="/detail/:studyId" component={Auth(StudyDetail, true)} /> 
+        <Route exact path="/myprofile" component={Auth(Myprofile, true)} /> 
+        <Route exact path="/profile/:profileId" component={Auth(Profile, true)} /> 
         <Route exact path="/uploadStudy" component={Auth(UploadStudyPost, true)} />
       </div>
     </Suspense>
