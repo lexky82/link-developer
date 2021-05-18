ef2e950c963b66c047982972e673f677dc305e2d
Kwon Hyeok Jin <lexky@naver.com>
2021-05-18 17:12:41 +0900

    StudyCard에서 요구 스킬까지 볼 수 있게 변경

--- client/src/Compontents/StudySearch/StudyCard.jsx
+++ client/src/Compontents/StudySearch/StudyCard.jsx
@@ -4,7 +4,6 @@ function StudyCard(props) {
 
     const { StudyPosts } = props
 
-
     return (
         <div>
             <div className="container">
@@ -23,8 +22,13 @@ function StudyCard(props) {
             <li className="notice__card">
                 <img src="https://img.icons8.com/ios/452/client-company.png" />
                 <h6><a href={`/detail/${props.particle._id}`}>{props.particle.title}</a></h6>
-                 <p>{props.particle.purpose}</p>
+                <p>{props.particle.purpose}</p>
                 <p>{props.particle.area}</p>
+                {
+                    props.particle.skill.map((a, i) => {
+                        return <span key={i} className="skillStackLabel">{a}</span>
+                    })
+                }
                 <p>시작일자 : {props.particle.date}</p>
             </li>
         )
