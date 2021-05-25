db18bb92039eda21a28dbcdaa57c568e3540f338
Kwon Hyeok Jin <lexky@naver.com>
2021-05-25 17:53:39 +0900

    Mainpage 컴포넌트 추가

--- client/src/Compontents/Mainpage.jsx
+++ client/src/Compontents/Mainpage.jsx
@@ -1,19 +1,44 @@
 import { Jumbotron, Button } from 'react-bootstrap';
 import React from "react";
+import { Carousel } from "antd";
 
 function Mainpage() {
     return (
         <div>
-                <Jumbotron className="mainpage">
-                    <div className="mainpage__content">
-                        <p>개발자 스터디,협업 알선 플랫폼</p>
-                        <p className="title">링크디벨로퍼가 개발자를 찾는데 도와줄게요.</p>
-                        <p>나와 다른 능력을 갖고있는 개발자들과 협업해보세요!</p>
-                        <Button href="/studySearch" className="mainpage__content-button">찾아보기</Button>
-                    </div>
-                    <img src="https://lh3.googleusercontent.com/proxy/pmKRj9S-hcO3wFdcWiv-BMrr0qKrKla1B06vUXxNosJs-3fUPofRdBba9TLoHj0mxWyRe3fFykyvOs2gTe_mTVm0KWpxscMlYmX2L0vVIGyz19vcFPAMkSpqMq_UAXG9TG4Jf9euYKKqkGuLbvCJWmE3uDX8nEVWZHMNGTEfCcFGhGgrPpEF" alt="" />
-                </Jumbotron>
+            <Jumbotron className="mainpage">
+                <div className="mainpage__content">
+                    <p>개발자 스터디, 협업 알선 플랫폼</p>
+                    <p className="title">링크디벨로퍼가 개발자를 찾는데 도와줄게요.</p>
+                    <p>나와 다른 능력을 갖고있는 개발자들과 협업해보세요!</p>
+                    <Button href="/studySearch" className="mainpage__content-button">찾아보기</Button>
+                </div>
+            </Jumbotron>
 
+            <section style={{ backgroundColor: '#4142dd', color: 'white' }}>
+                <div className="container" style={{ display: 'flex', paddingTop: '10rem' }}>
+                    <div style={{ textAlign: 'left', marginRight: '5rem' }}>
+                        <h3 style={{ fontSize: '30px', fontWeight: '500', lineHeight: '1.41', wordBreak: 'keep-all' }}>
+                            스터디를 찾고 있다면 링크디벨롭퍼를 적극 활용해보세요.
+                        </h3>
+                        <p style={{ fontSize: '22px', fontWeight: '200', lineHeight: '1.64', marginTop: '24px', wordBreak: 'keep-all' }}>본인이 사용하는 기술을 필요로 하는 스터디를 구하고
+                        지역을 검색하거나 온라인과 오프라인 여부를 선택하여 자신이 원하는 스터디를 찾을 수 있습니다.</p>
+                    </div>
+                    <Carousel dotPosition="top" style={{ width: '610px', maxWidth: '610px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} autoplay>
+                        <div>
+                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EC%8A%A4%ED%84%B0%EB%94%94%20%EC%B0%BE%EA%B8%B0.png?raw=true" style={{ width: '610px', maxWidth: '610px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} />
+                        </div>
+                        <div>
+                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EB%8F%99%EB%A3%8C%20%EC%B0%BE%EA%B8%B0.png?raw=true" style={{ width: '610px', maxWidth: '610px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} />
+                        </div>
+                        <div>
+                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EB%8F%99%EB%A3%8C%20%EC%B0%BE%EA%B8%B0.png?raw=true" style={{ width: '610px', maxWidth: '610px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} />
+                        </div>
+                        <div>
+                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EB%8F%99%EB%A3%8C%20%EC%B0%BE%EA%B8%B0.png?raw=true" style={{ width: '610px', maxWidth: '610px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} />
+                        </div>
+                    </Carousel>
+                </div>
+            </section>
         </div>
     )
 }
