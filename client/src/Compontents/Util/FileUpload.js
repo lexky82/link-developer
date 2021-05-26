1c3404c459124558b3dcd68d65896b54565f9c95
Kwon Hyeok Jin <lexky@naver.com>
2021-05-26 15:35:53 +0900

    프로필 이미지 업로드 컴포넌트 기존 Upload -> Dropzone 컴포넌트로 변경

--- client/src/Compontents/Util/FileUpload.js
+++ client/src/Compontents/Util/FileUpload.js
@@ -1,60 +1,36 @@
-import React, { useState } from 'react'
-import { Upload } from 'antd';
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react'
+import Icon, { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import axios from 'axios';
-
+import Dropzone from "react-dropzone";
 
 function FileUpload(props) {
-
-  const [Images, setImages] = useState('')
+  const { image, setImage } = props
   const [loading, setLoading] = useState(false)
 
-  function getBase64(img, callback) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.readAsDataURL(img);
+  const uploadChangeHandler = (file) => {
+    setLoading(true)
+    const formData = new FormData();
+
+    const config = {
+      header: { 'content-type': 'multipart/fomr-data' }
+    }
+
+    formData.append('file', file[0])
+    formData.append('_id', window.localStorage.getItem('userId'))
+
+    axios.post('http://localhost:5000/api/users/image', formData, config)
+      .then(response => {
+        if (response.data.success) {
+          setImage(response.data.file.path)
+          setLoading(false)
+        } else {
+          alert('이미지를 저장하는데 실패했습니다.')
+        }
+      })
   }
 
-  const beforeUpload = (file) => {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    if (!isJpgOrPng) {
-      alert('JPG. PNG 파일을 업로드 하세요.');
-    }
-
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-      alert('이미지 크기가 2MB가 넘습니다.');
-    }
-    
-    return isJpgOrPng && isLt2M;
-  }
-
-  const uploadChangeHandler = (info) => {
-
-    let body = {
-      _id : window.localStorage.getItem('userId'),
-    }
-
-  	axios.post('http://localhost:5000/api/users/image', body)
-    .then((response) => {
-      console.log('Rmx')
-    })
-
-    if (info.file.status === 'uploading') {
-      setLoading(true)
-      return;
-    }
-
-    if (info.file.status === 'done') {
-      getBase64(info.file.originFileObj, imageUrl =>
-        setImages(imageUrl),
-        setLoading(false),
-      );
-    }
-  }
-   
   const uploadButton = (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       {loading ? <LoadingOutlined /> : <PlusOutlined />}
       <div style={{ marginTop: 8 }}>프로필 이미지 업로드</div>
     </div>
@@ -62,19 +38,21 @@ function FileUpload(props) {
 
   return (
     <div>
-      <Upload
-        style={{ borderRadius: '70%' }}
-        name="file"
-        listType="picture-card"
-        className="avatar-uploader"
-        action="http://localhost:5000/api/users/image"
-        showUploadList={false}
-        beforeUpload={beforeUpload}
-        onChange={uploadChangeHandler}
-      >
-        {Images ? <img src={Images} alt="avatar" style={{ width: '100%', borderRadius: '10%' }} /> : uploadButton}
-      </Upload>
-    </div> 
+      <Dropzone onDrop={uploadChangeHandler} >
+        {({ getRootProps, getInputProps }) => (
+          <div style={{
+            border: 'lightgray',
+            display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%'
+          }}
+            {...getRootProps()}>
+            <input {...getInputProps()} />
+            {image ? <img src={image} alt="avatar" /> : uploadButton}
+          </div>
+        )}
+      </Dropzone>
+    </div>
+
+
   )
 }
 
