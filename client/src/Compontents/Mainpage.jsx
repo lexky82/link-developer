440bdc2ef78a9e4dce355817ebc41cd7a8b7b41a
Kwon Hyeok Jin <lexky@naver.com>
2021-05-26 17:33:15 +0900

    mainPage css 스마트폰 최적화 변경

--- client/src/Compontents/Mainpage.jsx
+++ client/src/Compontents/Mainpage.jsx
@@ -17,20 +17,20 @@ function Mainpage() {
             </Jumbotron>
 
             <section style={{ backgroundColor: '#4142dd', color: 'white' }}>
-                <div className="container" style={{ display: 'flex', paddingTop: '10rem' }}>
-                    <div style={{ textAlign: 'left', marginRight: '5rem' }}>
+                <div className="container wraper">
+                    <div className="wraper__text">
                         <h3 style={{ fontSize: '30px', fontWeight: '500', lineHeight: '1.41', wordBreak: 'keep-all' }}>
                             스터디를 찾고 있다면 링크디벨롭퍼를 적극 활용해보세요.
                         </h3>
                         <p style={{ fontSize: '22px', fontWeight: '200', lineHeight: '1.64', marginTop: '24px', wordBreak: 'keep-all' }}>본인이 사용하는 기술을 필요로 하는 스터디를 구하고
                         지역을 검색하거나 온라인과 오프라인 여부를 선택하여 자신이 원하는 스터디를 찾을 수 있습니다.</p>
                     </div>
-                    <Carousel dotPosition="top" style={{ width: '610px', maxWidth: '610px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} autoplay>
+                    <Carousel dotPosition="top" autoplay>
                         <div>
-                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EC%8A%A4%ED%84%B0%EB%94%94%20%EC%B0%BE%EA%B8%B0.png?raw=true" style={{ width: '610px', maxWidth: '610px',  height:'500px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} />
+                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EC%8A%A4%ED%84%B0%EB%94%94%20%EC%B0%BE%EA%B8%B0.png?raw=true" />
                         </div>
                         <div>
-                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EB%8F%99%EB%A3%8C%20%EC%B0%BE%EA%B8%B0.png?raw=true" style={{ width: '610px', maxWidth: '610px', height:'500px', maxHeight: '500px', boxShadow: '0 0 21px 6px rgb(31 45 61 / 50%)' }} />
+                            <img src="https://github.com/lexky82/link-developer/blob/main/client/image/%EB%8F%99%EB%A3%8C%20%EC%B0%BE%EA%B8%B0.png?raw=true" />
                         </div>
                     </Carousel>
                 </div>
