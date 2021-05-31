d8dd8f0175cd19330f8ef57d9221c930a105c499
Kwon Hyeok Jin <lexky@naver.com>
2021-05-31 17:39:02 +0900

    카드 디자인 개선 및 UploadStudy 컴포넌트 이동할 수 있게 구현

--- client/src/Compontents/StudySearch/StudyCard.jsx
+++ client/src/Compontents/StudySearch/StudyCard.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+/* Components  */
+import { UploadOutlined, EnvironmentOutlined } from '@ant-design/icons'
+
 /* image */
 import imgStudy from '../../image/Study.png'
 
@@ -12,6 +15,14 @@ function StudyCard(props) {
         <div>
             <div >
                 <ul className="notice">
+                    <li className="notice__card" >
+                        <Link to="uploadStudy" style={{ textDecoration: 'none', color: 'black' }}>
+                                <UploadOutlined style={{ fontSize: '100px', padding:'2rem', border: '1px solid black', borderRadius: '50%' }} />
+                                <div>
+                                    <p className="title">스터디 업로드</p>
+                                </div>
+                        </Link>
+                    </li>
                     {
                         StudyPosts.map((a, i) => {
                             return <CardNotice key={i} particle={StudyPosts[i]} />
@@ -26,9 +37,12 @@ function StudyCard(props) {
         return (
             <li className="notice__card">
                 <img src={imgStudy} />
-                <p className="notice__card--title"><Link to={`/detail/${props.particle._id}`}>{props.particle.title}</Link></p>
-                <p>{props.particle.purpose}</p>
-                <p>{props.particle.area}</p>
+                <Link className="notice__card--title" to={`/detail/${props.particle._id}`}>{props.particle.title}</Link>
+                <p className="projectName">{props.particle.purpose}</p>
+                <div className="projectName">
+                    <EnvironmentOutlined style={{marginRight:'2px'}}/>
+                    {props.particle.area}
+                </div>
                 {
                     props.particle.skill.map((skill, i) => {
                         return <span key={i} className="skillStackLabel">{skill}</span>
