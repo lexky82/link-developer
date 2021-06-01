9f92c22d4f6144b6a024e131146e7baa5518b3d3
Kwon Hyeok Jin <lexky@naver.com>
2021-06-01 17:59:38 +0900

    docs: Types 통합으로 주석처리
    기존 reducer 단위로 나눠져 있던 Types 통합으로 주석처리로 구분할 수 있게 변경

--- client/src/_actions/Types.js
+++ client/src/_actions/Types.js
@@ -1,8 +1,11 @@
+/* USER */
 export const LOGIN_USER = 'login_user';
 export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'auth_user';
 export const LOGOUT_USER = 'logout_user';
 
+/* STDUY */
 export const STDUY_LIST = 'study_list';
 
+/* UserINFO */
 export const USER_INFO = 'user_info';
