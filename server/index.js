fc4bb02995992b60a38499f06414a2181938c02c
Kwon Hyeok Jin <lexky@naver.com>
2021-06-10 13:00:20 +0900

    js -> jsx로 확장자명 변경

--- server/index.js
+++ server/index.js
@@ -45,14 +45,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
 });
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
-
-}
-
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
