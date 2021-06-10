dedfe492c3c00ffded0ed57afb97ed73752f887d
Kwon Hyeok Jin <lexky@naver.com>
2021-06-10 12:06:19 +0900

    refactor: Route 되는 컴포넌트 page 디렉토리 이동으로 Import 경로 변경

--- client/src/Compontents/App.js
+++ client/src/Compontents/App.js
@@ -2,40 +2,42 @@ import '../css/App.css';
 import React, { Suspense } from 'react';
 
 /* Lib */
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 
 /* Components */
 import NavBar from "./NavBar";
-import Mainpage from "./Mainpage";
-import Login from './LoginPage/LoginPage';
-import Regiseter from "./RegisterPage/RegisterPage";
-import StudySearch from "./StudySearch/StudySearch";
-import FriendSearch from "./FriendSearch/FriendSearch";
-import Myprofile from "./Profile/Myprofile";
-import UploadStudyPost from "./UploadStudyPost";
+import Mainpage from "../pages/Mainpage";
+import Login from '../pages/LoginPage';
+import Regiseter from "../pages/RegisterPage";
+import StudySearch from "../pages/StudySearch";
+import FriendSearch from "../pages/FriendSearch";
+import Myprofile from "../pages/Myprofile";
+import UploadStudyPost from "../pages/UploadStudyPost";
 import StudyDetail from "./StudySearch/StudyDetail";
-import Profile from "./Profile/Profile"
+import Profile from "../pages/Profile"
 import Auth from './HOC/auth'
 import Footer from './Footer'
 
 function App() {
 
   return (
-    <Suspense fallback={(<div>Loading...</div>)}>
-      <NavBar />
-      <div style={{ textAlign: 'center', minHeight: 'calc(100vh - 80px)' }}>
-        <Route exact path="/" component={Auth(Mainpage, null)} />
-        <Route exact path="/login" component={Auth(Login, false)} />
-        <Route exact path="/signup" component={Auth(Regiseter, false)} />
-        <Route exact path="/studySearch" component={Auth(StudySearch, null)} />
-        <Route exact path="/friendSearch" component={Auth(FriendSearch, null)} />
-        <Route exact path="/detail/:studyId" component={Auth(StudyDetail, null)} />
-        <Route exact path="/myprofile" component={Auth(Myprofile, true)} />
-        <Route exact path="/profile/:profileId" component={Auth(Profile, null)} />
-        <Route exact path="/uploadStudy" component={Auth(UploadStudyPost, true)} />
-      </div>
-      <Footer />
-    </Suspense>
+    <Switch>
+      <Suspense fallback={(<div>Loading...</div>)}>
+        <NavBar />
+        <div style={{ textAlign: 'center', minHeight: 'calc(100vh - 80px)' }}>
+          <Route exact path="/" component={Auth(Mainpage, null)} />
+          <Route exact path="/login" component={Auth(Login, false)} />
+          <Route exact path="/signup" component={Auth(Regiseter, false)} />
+          <Route exact path="/studySearch" component={Auth(StudySearch, null)} />
+          <Route exact path="/friendSearch" component={Auth(FriendSearch, null)} />
+          <Route exact path="/detail/:studyId" component={Auth(StudyDetail, null)} />
+          <Route exact path="/myprofile" component={Auth(Myprofile, true)} />
+          <Route exact path="/profile/:profileId" component={Auth(Profile, null)} />
+          <Route exact path="/uploadStudy" component={Auth(UploadStudyPost, true)} />
+        </div>
+        <Footer />
+      </Suspense>
+    </Switch>
   );
 }
 
