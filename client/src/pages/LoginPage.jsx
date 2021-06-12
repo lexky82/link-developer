9c57489269fa6044c0cec2a74fe1dfdcb1381576
Kwon Hyeok Jin <80798626+lexky82@users.noreply.github.com>
2021-06-13 02:26:25 +0900

    refactor: 로그인, 회원가입 재구현
    기존에 Formik, Yup을 복붙으로 가져와 사용하던 것 전부 삭제 및 직접 구현

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,163 +1,139 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import { Link, withRouter } from 'react-router-dom';
 
 /* Lib */
-import { withRouter } from "react-router-dom";
 import { loginUser } from "../_actions/user_actions";
-import * as Yup from 'yup';
 import { useDispatch } from "react-redux";
 
 /* Components */
-import { Form, Input, Button, Checkbox, Typography } from 'antd';
-import Icon, { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Formik } from 'formik';
+import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
 
+import {
+  Form,
+  Input,
+  Button,
+  Typography,
+  Checkbox,
+  notification 
+} from 'antd';
 
 const { Title } = Typography;
 
 function LoginPage(props) {
-
   const dispatch = useDispatch();
+
   const rememberMeChecked = localStorage.getItem("rememberMe") ? true : false;
 
-  const [formErrorMessage, setFormErrorMessage] = useState('')
+  const initialEmail = localStorage.getItem("rememberMe")
+  ? localStorage.getItem("rememberMe")
+  : '';
+
+  const [email, setEmail] = useState(initialEmail);
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState('')
   const [rememberMe, setRememberMe] = useState(rememberMeChecked)
 
-  const handleRememberMe = () => {
-    setRememberMe(!rememberMe)
+  const openNotification = (err) => {
+    notification.open({
+      message: 'Login Error',
+      description:
+        err
+        ? err
+        : '이메일 또는 비밀번호를 확인하시길 바랍니다.',
+      
+      icon: <UserOutlined style={{ color: 'red' }} />,
+      placement: 'topLeft'
+    });
   };
 
-  // ID 저장 체크 및 저장
-  const initialEmail = localStorage.getItem("rememberMe")
-    ? localStorage.getItem("rememberMe")
-    : '';
+  const emailCheck = useCallback(email => {
+    const regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    return (email != '' && email != 'undefined' && regex.test(email));
+  }, [email])
+  
+  const handleRememberMe = useCallback(e => {
+    setRememberMe(!rememberMe)
+  }, [rememberMe]);
+
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, [email])
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, [password])
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    if (emailCheck(email) && password) {
+      setLoginError('')
+
+      let dataToSubmit = {
+        email: email,
+        password: password,
+      };
+
+      dispatch(loginUser(dataToSubmit))
+        .then(response => {
+          if (response.payload.loginSuccess) {
+            window.localStorage.setItem('userId', response.payload.userId);
+            if (rememberMe === true) {
+              window.localStorage.setItem('rememberMe', email);
+            } else {
+              localStorage.removeItem('rememberMe');
+            }
+            props.history.push("/");
+          } else {
+            openNotification();
+          }
+        })
+        .catch(err => {
+          openNotification(err);
+        });
+    }
+  }, [email, password])
 
   return (
-    <Formik
+    <div style={{ maxWidth: "25rem", margin: '10rem auto' }}>
+      <Title>로그인</Title>
 
-      // 입력하지 않았을때 값
-      initialValues={{
-        email: initialEmail,
-        password: '',
-      }}
+      <Form onSubmit={onSubmit}>
+        <Form.Item required>
+          <Input
+            id="email"
+            placeholder="email"
+            type="email"
+            value={email}
+            onChange={onChangeEmail}
+            prefix={<MailOutlined className="site-form-item-icon" />}
+          />
+          {!emailCheck(email) && <div>이메일 형식을 입력하세요!</div>}
+        </Form.Item>
 
-      // 입력 값 유효성 검증
-      validationSchema={Yup.object().shape({
-        email: Yup.string()
-          .email('이메일 형식이 아닙니다.')
-          .required('이메일을 입력하지 않았습니다.'),
-        password: Yup.string()
-          .min(6, '패스워드는 6자리 이상 입력해주세요.')
-          .required('패스워드를 입력하지 않았습니다.'),
-      })}
+        <Form.Item required>
+          <Input
+            id="password"
+            placeholder="비밀번호"
+            type="password"
+            value={password}
+            onChange={onChangePassword}
+            prefix={<LockOutlined className="site-form-item-icon" />}
+          />
+        </Form.Item>
+        {loginError && <div>{loginError}</div>}
+        <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe}>이메일 저장</Checkbox>
 
-      onSubmit={(values, { setSubmitting }) => {
-
-        setTimeout(() => {
-          let dataToSubmit = {
-            email: values.email,
-            password: values.password
-          };
-
-          dispatch(loginUser(dataToSubmit))
-            .then(response => {
-              if (response.payload.loginSuccess) {
-                window.localStorage.setItem('userId', response.payload.userId);
-                if (rememberMe === true) {
-                  window.localStorage.setItem('rememberMe', values.email);
-                } else {
-                  localStorage.removeItem('rememberMe');
-                }
-                props.history.push("/");
-              } else {
-                setFormErrorMessage('이메일 또는 패스워드를 확인해주시기 바랍니다.')
-              }
-            })
-            .catch(err => {
-              setFormErrorMessage('이메일 또는 패스워드를 확인해주시기 바랍니다.')
-              setTimeout(() => {
-                setFormErrorMessage("")
-              }, 3000);
-            });
-          setSubmitting(false);
-        }, 500);
-      }}
-    >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        } = props;
-
-        return (
-          <div style={{ maxWidth: "350px", margin: '10rem auto' }}>
-
-            <Title level={2}>로그인</Title>
-            <form onSubmit={handleSubmit} >
-
-              <Form.Item required>
-                <Input
-                  id="email"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="email"
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  prefix={<UserOutlined className="site-form-item-icon" />}
-                  className={
-                    errors.email && touched.email ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required>
-                <Input
-                  id="password"
-                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="password"
-                  type="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  className={
-                    errors.password && touched.password ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.password && touched.password && (
-                  <div className="input-feedback">{errors.password}</div>
-                )}
-              </Form.Item>
-
-              {formErrorMessage && (
-                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
-              )}
-
-              <Form.Item>
-                <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >이메일 저장</Checkbox>
-                <div>
-                  <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                    로그인
-                </Button>
-                </div>
-                <a href="/signup">회원가입 하기</a>
-              </Form.Item>
-            </form>
-          </div>
-        );
-      }}
-    </Formik>
-  );
+        <Button type="primary" style={{ minWidth: '100%' }} onClick={onSubmit} >
+          로그인
+        </Button>
+      </Form>
+      <div>
+        회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하기</Link>
+      </div>
+    </div>
+  )
 };
 
-export default withRouter(LoginPage);
-
-
+export default withRouter(LoginPage)
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { Link } from 'react-router-dom';
 
 /* Lib */
-import * as Yup from 'yup';
 import { registerUser } from "../_actions/user_actions";
 import { useDispatch } from "react-redux";
 
 /* Components */
-import { Formik } from 'formik';
 import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
 
 import {
@@ -17,174 +16,127 @@ import {
 } from 'antd';
 
 const { Title } = Typography;
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
-  },
-};
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
 
 function RegisterPage(props) {
   const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('')
+  
+  const emailCheck = useCallback(email => {    
+    const regex=/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    return (email != '' && email != 'undefined' && regex.test(email)); 
+  }, [email])
+
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, [email])
+
+  const onChangeName = useCallback((e) => {
+    setName(e.target.value);
+  }, [name])
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+    setMismatchError(e.target.value !== passwordCheck)
+  }, [passwordCheck])
+
+  const onChangePasswordCheck = useCallback((e) => {
+    setPasswordCheck(e.target.value);
+    setMismatchError(e.target.value !== password)
+  }, [password])
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+    
+    if(!mismatchError && name && emailCheck(email) && !(password.length < 6)){
+      setSignUpError('')
+
+      let dataToSubmit = {
+        email: email,
+        password: password,
+        name: name,
+      };
+
+      dispatch(registerUser(dataToSubmit)).then(response => {
+        if (response.payload.success) {
+          props.history.push("/login");
+        } else {
+          setSignUpError(response.payload.message)
+        }
+      })
+    }
+  }, [email, name, password, passwordCheck])
+  
   return (
+    <div style={{ maxWidth: "25rem", margin: '10rem auto' }}> 
+      <Title>회원가입</Title>
 
-    <Formik
-      initialValues={{
-        email: '',
-        name: '',
-        password: '',
-        confirmPassword: ''
-      }}
-      validationSchema={Yup.object().shape({
-        name: Yup.string()
-          .required('이름을 입력하지 않았습니다.'),
-        email: Yup.string()
-          .email('이메일 형태가 아닙니다.')
-          .required('이메일을 입력하지 않았습니다.'),
-        password: Yup.string()
-          .min(6, '패스워드는 6자리 이상 입력해주세요.')
-          .required('패스워드를 입력하지 않았습니다.'),
-        confirmPassword: Yup.string()
-          .oneOf([Yup.ref('password'), null], '패스워드와 일치하지 않습니다.')
-          .required('패스워드 확인을 입력하지 않았습니다.')
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
+      <Form onSubmit={onSubmit}>
+        <Form.Item required>
+          <Input
+            id="email"
+            placeholder="email"
+            type="email"
+            value={email}
+            onChange={onChangeEmail}
+            prefix={<MailOutlined className="site-form-item-icon" />}
+          />
+          {!emailCheck(email) && <div>이메일 형식을 입력하세요!</div>}
+          </Form.Item>
+          
+          <Form.Item required>
+          <Input
+            id="name"
+            placeholder="닉네임"
+            type="text"
+            value={name}
+            onChange={onChangeName}
+            prefix={<UserOutlined className="site-form-item-icon" />}
+          />
+          {!name && <div>닉네임을 입력해주세요!</div>}
+          </Form.Item>
 
-          let dataToSubmit = {
-            email: values.email,
-            password: values.password,
-            name: values.name,
-            lastname: values.lastname,
-          };
+          <Form.Item required>
+          <Input
+            id="password"
+            placeholder="비밀번호"
+            type="password"
+            value={password}
+            onChange={onChangePassword}
+            prefix={<LockOutlined className="site-form-item-icon" />}
+          />
+          {password.length < 6 && <div>비밀번호를 6자리 이상</div>}
+          </Form.Item>
 
-          dispatch(registerUser(dataToSubmit)).then(response => {
-            if (response.payload.success) {
-              props.history.push("/login");
-            } else {
-              alert(response.payload.message)
-            }
-          })
+          <Form.Item required>
+          <Input
+            id="passwordCheck"
+            placeholder="비밀번호 확인"
+            type="password"
+            value={passwordCheck}
+            onChange={onChangePasswordCheck}
+            prefix={<LockOutlined className="site-form-item-icon" />}
+          />
+          {mismatchError && <div>비밀번호가 일치하지 않습니다.</div>}
+          </Form.Item>
 
-          setSubmitting(false);
-        }, 500);
-      }}
-    >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        } = props;
-        return (
-          <div className="app">
-
-            <Form style={{ maxWidth: "400px", margin: '10rem auto' }} {...formItemLayout} onSubmit={handleSubmit} >
-              <Title level={2}>회원가입</Title>
-
-              <Form.Item required label="이름">
-                <Input
-                  id="name"
-                  placeholder="이름"
-                  type="text"
-                  value={values.name}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  prefix={<UserOutlined className="site-form-item-icon" />}
-                  className={
-                    errors.name && touched.name ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.name && touched.name && (
-                  <div className="input-feedback">{errors.name}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required label="Email" hasFeedback validateStatus={errors.email && touched.email ? "error" : 'success'}>
-                <Input
-                  id="email"
-                  placeholder="Email Adress"
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  prefix={<MailOutlined />}
-                  className={
-                    errors.email && touched.email ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required label="비밀번호" hasFeedback validateStatus={errors.password && touched.password ? "error" : 'success'}>
-                <Input
-                  id="password"
-                  placeholder="비밀번호"
-                  type="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  className={
-                    errors.password && touched.password ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.password && touched.password && (
-                  <div className="input-feedback">{errors.password}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required label="비밀번호 확인" hasFeedback>
-                <Input
-                  id="confirmPassword"
-                  placeholder="비밀번호 다시 입력해주세요"
-                  type="password"
-                  value={values.confirmPassword}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  className={
-                    errors.confirmPassword && touched.confirmPassword ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.confirmPassword && touched.confirmPassword && (
-                  <div className="input-feedback">{errors.confirmPassword}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item {...tailFormItemLayout}>
-                <Button onClick={handleSubmit} type="primary" disabled={isSubmitting}>
-                  회원가입
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
-        );
-      }}
-    </Formik>
-  );
+          {signUpError && <div>{signUpError}</div>}
+        <Button type="primary" style={{ minWidth: '100%' }} onClick={onSubmit} >
+          회원가입
+        </Button>
+      </Form>
+      <div>
+        이미 회원이신가요?&nbsp;
+        <Link to="/login">로그인 하러가기</Link>
+      </div>
+    </div>
+  )
+  
 };
 
 export default RegisterPage
